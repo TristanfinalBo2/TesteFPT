3a000c22afd5bace6bc668176a70351030ab1a6c
TristanfinalBo2 <tsplayer18@gmail.com>
2024-12-02 22:38:59 +0200

    b

--- api/index.js
+++ api/index.js
@@ -188,7 +188,8 @@ app.get("/test", (req, res) => {
             html = html.replace("{{TEST_TYPE}}", testType)
                        .replace("{{TEST_CODE}}", code)
                        .replace("{{TEST_NAME}}", name)
-                       .replace("{{TEST_DISCORD}}", discordId);
+                       .replace("{{TEST_DISCORD}}", discordId)
+                       .replace("{{TEST_TYPE2}}", testType); 
 
             // Send the modified HTML to the client
             return res.send(html);
