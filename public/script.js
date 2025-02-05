a54983ba9cea77ead98803cba88ecd06ba6cfe5a
TristanfinalBo2 <tsplayer18@gmail.com>
2025-02-05 21:54:42 +0100

    Update script.js

--- public/script.js
+++ public/script.js
@@ -248,7 +248,7 @@ document.addEventListener("DOMContentLoaded", () => {
         const now = new Date().getTime();
         const distance = countDownDate - now;
         const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-        const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+        const seconds = Math.floor((distance % (1000 * 60)) / 1000)
         const resultData = {
             discordId: discordId,
             testType: testType,
